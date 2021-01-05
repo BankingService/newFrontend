3ff89b86e1271e5498e824d81ef7452e6eb6fea4
vivek7415 <viveksngh443@gmail.com>
2021-01-05 19:56:35 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NeftComponent } from './components/neft/neft.component';
+import { RtgsComponent } from './components/rtgs/rtgs.component';
+import { ImpsComponent } from './components/imps/imps.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NeftComponent,
+    RtgsComponent,
+    ImpsComponent
   ],
   imports: [
     BrowserModule,
