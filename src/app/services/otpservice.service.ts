f41f1d4a003246a5236763fc3156e3c37df99041
nishitajain99 <jainnishita99@gmail.com>
2021-01-15 14:06:44 +0530

    Forgot Password OTP

---
+++ src/app/services/otpservice.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OtpserviceService {
+
+  constructor(private http:HttpClient) { }
+
+  getOtp(id):Observable<any>{
+  let url = "http://localhost:8086/forgotpassword/"+id;
+  return this.http.get<any>(url);
+  }
+}
