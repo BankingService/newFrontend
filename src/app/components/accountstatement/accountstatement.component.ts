ad9217b4cf14d9a8c82129d1a0aa2c1d2ec84242
guhii10 <guhenle@gmail.com>
2021-01-15 10:03:30 +0530

    transaction statement changes

--- src/app/components/accountstatement/accountstatement.component.ts
+++ src/app/components/accountstatement/accountstatement.component.ts
@@ -14,7 +14,7 @@ import { TransactionstatementService } from 'src/app/services/transactionstateme
 export class AccountstatementComponent implements OnInit {
   form: FormGroup;
   transactiondatetime: Transactiondatetime;
-  transactionstatement: TransactionStatement[] = [];
+  transactionstatement: any = [];
 
   error_messages = {
 
@@ -57,6 +57,7 @@ export class AccountstatementComponent implements OnInit {
 
     this.transaction.createTransactionRequest(this.transactiondatetime).subscribe((data: {}) => {
       alert(JSON.stringify(data))
+      this.transactionstatement.push(data);
     })
     // location from ip address
     // https://www.melissa.com/v2/lookups/iplocation/ip/223.182.242.158?fmt=json&id=
